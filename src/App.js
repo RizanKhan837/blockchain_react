cc797b85cf501bb7d586008ae479e045edd4a669
rizankhan <rizankhan837@gmail.com>
2022-10-30 12:01:28 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //let a = 'abc';
+  //let b = 10
+  //let c = ['a', 'b', 'c', 'd']
+
+  let object = [
+    {
+      name: 'Rizwan', 
+      age: 10
+    },
+    {
+      name: 'Usman',
+      age: 4
+    },
+    {
+      name: 'Hannan',
+      age: 13
+    }
+  ]
+  let arr = [];
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>RizanKhan Blockchain</h1>
+        {/* <p>{a}</p> */}
+
+        {/* <p>The Square Of {b} is {b}*{b}</p> */}
+
+        {/* <p>{c}</p> */}
+        {/* <ul>
+          {
+            c.map((x, i) => { // yhn hmny i ko unique key use kiya
+              return <li key={i}> {x}</li>
+            })
+          }
+        </ul> */}
+
+        {/* <ol>
+          {object.map((x, i) => {
+            return <li key={i}> {x.name} is {x.age} years old</li>
+          })}
+        </ol> */}
+
+
+
+
+
       </header>
     </div>
   );
